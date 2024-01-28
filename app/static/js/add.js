4d81969ab6d92ab29e0748e9607698c09997f7fb
Przemyslaw.Kozak.QA <przemyslaw.kozak.qa@gmail.com>
2024-01-28 16:29:06 +0100

    typo corrections

--- app/static/js/add.js
+++ app/static/js/add.js
@@ -7,12 +7,12 @@ $(document).ready(function () {
     $('#submitBtn').on('click', function (event) {
         let word = $('#word').val().trim();
         let definition = $('#def').val().trim();
-        //getting the maximum length from the 'maxlength' attribute of the input
-        let maxlength = $('#word').attr("maxlength");
+        //getting the maximum length from the 'maxLength' attribute of the input
+        let maxLength = $('#word').attr("maxlength");
         if (word.length < 1) {
             displayWarning('Word/concept can not be empty!');
-        } else if (word.length > maxlength) {
-            displayWarning(`Maximum word/concept length is ${maxlength} characters!`);
+        } else if (word.length > maxLength) {
+            displayWarning(`Maximum word/concept length is ${maxLength} characters!`);
         } else if (definition.length < 1) {
             displayWarning('Definition/meaning can not be empty!');
         } else {
@@ -25,10 +25,10 @@ $(document).ready(function () {
 // script prevents entering too long text into the input
 $(document).ready(function () {
     $('#word').on('change', function (event) {
-        //getting the maximum length from the 'maxlength' attribute of the input
-        let maxlength = $(this).attr("maxlength");
+        //getting the maximum length from the 'maxLength' attribute of the input
+        let maxLength = $(this).attr("maxlength");
         let text = $(this).val();
-        if (text.length >= maxlength) {
+        if (text.length >= maxLength) {
             $(this).value = '';
         }
     });
