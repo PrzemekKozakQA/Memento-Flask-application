3415d29f7111e9e8bd3405a1c1afd77f667f94e9
Przemyslaw.Kozak.QA <przemyslaw.kozak.qa@gmail.com>
2024-02-20 20:29:41 +0100

    CI test

--- app/app.py
+++ app/app.py
@@ -14,6 +14,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///memento.db")
 
